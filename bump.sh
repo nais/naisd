31e4f5c4b738c7bb50eb5c982d2927adad585979
Johnny Horvi <johnny.horvi@gmail.com>
2017-07-10 15:22:00 +0200

    test travis skip [skip ci]

--- bump.sh
+++ bump.sh
@@ -4,7 +4,7 @@ OLD=$(cat ./version | cut -d'.' -f1)
 NEW=$(expr $OLD + 1)
 echo "$NEW.0.0" > version
 git add version
-git commit -am "increased version number to $NEW, skip ci"
+git commit -am "increased version number to $NEW [skip ci]"
 git push origin master
 git tag $NEW -m "$NEW"
 git push --tags

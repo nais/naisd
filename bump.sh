164fc123b0b56b06028a9d9c6d5e73ae036fc6ea
Johnny Horvi <johnny.horvi@gmail.com>
2017-07-10 14:53:24 +0200

    semantic versioning sa folk

--- bump.sh
+++ bump.sh
@@ -2,7 +2,7 @@
 
 OLD=$(cat ./version | cut -d'.' -f1)
 NEW=$(expr $OLD + 1)
-echo $NEW > version
+echo "$NEW.0.0" > version
 git add version
 git commit -am "increased version number to $NEW, skip ci"
 git push origin master

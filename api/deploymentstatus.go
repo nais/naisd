f26d0e948e3c1e6ac48e6412d82273cef61ec081
Giao The Cung <giaothe@gmail.com>
2017-09-27 14:49:36 +0200

    Calculate deployment status.

--- api/deploymentstatus.go
+++ api/deploymentstatus.go
@@ -13,7 +13,7 @@ func deploymentStatusHandler(clientset kubernetes.Interface) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		namespace, appName := parseURLPath(r.URL)
 		if dep, err := clientset.ExtensionsV1beta1().Deployments(namespace).Get(appName); err != nil {
-			_, deployFinished, err := status(dep)
+			_, deployFinished, err := isDeploymentFinished(dep)
 
 			if err != nil {
 				w.WriteHeader(http.StatusInternalServerError)
@@ -30,23 +30,25 @@ func deploymentStatusHandler(clientset kubernetes.Interface) http.Handler {
 	})
 
 }
-func status(deployment *v1beta1.Deployment) (string, bool, error) {
+func isDeploymentFinished(deployment *v1beta1.Deployment) (string, bool, error) {
 	if deployment.Generation <= deployment.Status.ObservedGeneration {
 		if deploymentExceededProgressDeadline(deployment.Status) {
 			return "", false, fmt.Errorf("deployment %q exceeded its progress deadline", deployment.Name)
 		}
 		if deployment.Status.UpdatedReplicas < *deployment.Spec.Replicas {
-			return fmt.Sprintf("Waiting for rollout to finish: %d out of %d new replicas have been updated...\n", deployment.Status.UpdatedReplicas, deployment.Spec.Replicas), false, nil
+			return fmt.Sprintf("Waiting for rollout to finish: %d out of %d new replicas have been updated.", deployment.Status.UpdatedReplicas, deployment.Spec.Replicas), false, nil
 		}
 		if deployment.Status.Replicas > deployment.Status.UpdatedReplicas {
-			return fmt.Sprintf("Waiting for rollout to finish: %d old replicas are pending termination...\n", deployment.Status.Replicas-deployment.Status.UpdatedReplicas), false, nil
+			return fmt.Sprintf("Waiting for rollout to finish: %d old replicas are pending termination.", deployment.Status.Replicas-deployment.Status.UpdatedReplicas), false, nil
 		}
 		if deployment.Status.AvailableReplicas < deployment.Status.UpdatedReplicas {
-			return fmt.Sprintf("Waiting for rollout to finish: %d of %d updated replicas are available...\n", deployment.Status.AvailableReplicas, deployment.Status.UpdatedReplicas), false, nil
+			return fmt.Sprintf("Waiting for rollout to finish: %d of %d updated replicas are available.", deployment.Status.AvailableReplicas, deployment.Status.UpdatedReplicas), false, nil
 		}
-		return fmt.Sprintf("deployment %q successfully rolled out\n", deployment.Name), true, nil
+		return fmt.Sprintf("deployment %q successfully rolled out.", deployment.Name), true, nil
 	}
+	return fmt.Sprintf("Waiting for deployment spec update to be observed."), false, nil
 }
+
 func deploymentExceededProgressDeadline(status v1beta1.DeploymentStatus) bool {
 	for i := range status.Conditions {
 		c := status.Conditions[i]
--- api/deploymentstatus_test.go
+++ api/deploymentstatus_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 	"github.com/stretchr/testify/assert"
 	"net/url"
+	"k8s.io/client-go/pkg/apis/extensions/v1beta1"
+	"k8s.io/client-go/pkg/api/v1"
 )
 
 func TestAppnNameFromUrl(t *testing.T) {
@@ -13,4 +15,107 @@ func TestAppnNameFromUrl(t *testing.T) {
 
 	assert.Equal(t, "appname", appName)
 	assert.Equal(t, "namespace", namespace)
+}
+
+func TestIsDeploymentFinished(t *testing.T) {
+
+	var wantedReplicas int32 = 2
+	var deploymentGeneration int64 = 2
+
+	deployment := &v1beta1.Deployment{
+		ObjectMeta: v1.ObjectMeta{
+			Name:       "appname",
+			Namespace:  "default",
+			Generation: deploymentGeneration,
+		},
+		Spec: v1beta1.DeploymentSpec{
+			Replicas: int32p(wantedReplicas),
+		},
+	}
+
+	t.Run("Error when progress dead line is exceeded", func(t *testing.T) {
+
+		deployment.Status = v1beta1.DeploymentStatus{
+			ObservedGeneration: deploymentGeneration,
+			Conditions: []v1beta1.DeploymentCondition{
+				{
+					Type:   v1beta1.DeploymentProgressing,
+					Reason: "ProgressDeadlineExceeded",
+				},
+			},
+		}
+
+		_, isFinished, err := isDeploymentFinished(deployment)
+
+		assert.Error(t, err)
+		assert.Equal(t, false, isFinished)
+	})
+
+	t.Run("Test for when a deployment is finished", func(t *testing.T) {
+		tests := []struct {
+			testDescription string
+			deployStatus    v1beta1.DeploymentStatus
+			expectedStatus  bool
+		}{
+			{
+				testDescription: "Deploy is not finished when observed generation is less than spec generation.",
+				deployStatus: v1beta1.DeploymentStatus{
+					ObservedGeneration: deploymentGeneration - 1,
+				},
+				expectedStatus: false,
+			},
+			{
+				testDescription: "Deploy is not finished when updated replicas are less than wanted replicas",
+				deployStatus: v1beta1.DeploymentStatus{
+					ObservedGeneration: deploymentGeneration,
+					UpdatedReplicas:    wantedReplicas - 1,
+					AvailableReplicas:  wantedReplicas,
+				},
+				expectedStatus: false,
+			},
+			{
+				testDescription: "Deploy is not finished when there are more replicas  than wanted replicas",
+				deployStatus: v1beta1.DeploymentStatus{
+					ObservedGeneration: deploymentGeneration,
+					UpdatedReplicas:    wantedReplicas,
+					AvailableReplicas:  wantedReplicas,
+					Replicas:           wantedReplicas + 1,
+				},
+				expectedStatus: false,
+			},
+			{
+
+				testDescription: "Deploy is not finished when there are less available replicas than wanted replicas",
+				deployStatus: v1beta1.DeploymentStatus{
+					ObservedGeneration: deploymentGeneration,
+					UpdatedReplicas:    wantedReplicas,
+					AvailableReplicas:  wantedReplicas - 1,
+					Replicas:           wantedReplicas,
+				},
+				expectedStatus: false,
+			},
+			{
+
+				testDescription: "Deploy is finished when the number of replicas, available, updated and wanted replicas are equal",
+				deployStatus: v1beta1.DeploymentStatus{
+					ObservedGeneration: deploymentGeneration,
+					UpdatedReplicas:    wantedReplicas,
+					AvailableReplicas:  wantedReplicas,
+					Replicas:           wantedReplicas,
+				},
+				expectedStatus: true,
+			},
+		}
+
+		for _, test := range tests {
+			deployment.Status = test.deployStatus
+
+			_, actualStatus, _ := isDeploymentFinished(deployment)
+			if test.expectedStatus != actualStatus {
+				t.Errorf("Failed test: %s\n DeploymentStatus: %+v", test.testDescription, test.deployStatus)
+			}
+		}
+
+	})
+
 }

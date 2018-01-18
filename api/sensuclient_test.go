9680bdfba40199d5ea06702d53226b7783541442
Kyrre Havik Eriksen <kyrre.havik.eriksen@nav.no>
2018-01-18 12:10:58 +0100

    Using NaisDeploymentRequest in test

--- api/sensuclient_test.go
+++ api/sensuclient_test.go
@@ -8,12 +8,20 @@ import (
 
 func TestSensuClient(t *testing.T) {
 	t.Run("Check generated deploy message", func(t *testing.T) {
-		application := "TestApp"
+		deploymentRequest := NaisDeploymentRequest{
+			"TestApp",
+			"42.0.0",
+			"environment",
+			"zone",
+			"appconfigurl",
+			"username",
+			"password",
+			"onbehalfof",
+			"nais",
+		}
 		clusterName := "nais-dev"
-		namespace := "nais"
-		version := "42.0.0"
 
-		message, err := GenerateDeployMessage(&application, &clusterName, &namespace, &version)
+		message, err := GenerateDeployMessage(&deploymentRequest, &clusterName)
 		assert.NoError(t, err)
 		expectedMessagePrefix := "{\"name\":\"naisd.deployment\",\"type\":\"metric\",\"handlers\":[\"events_nano\"],\"output\":\"naisd.deployment,application=TestApp,clusterName=nais-dev,namespace=nais version=\\\"42.0.0\\\""
 		assert.Equal(t, true, strings.HasPrefix(string(message), expectedMessagePrefix))

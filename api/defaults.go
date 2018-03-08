fd59e456c31d77c0710aaad34845a9c58272e5e6
Kyrre Havik Eriksen <kyrremann@gmail.com>
2018-03-08 15:47:39 +0100

    Added RedisFailover resource creator

--- api/defaults.go
+++ api/defaults.go
@@ -60,6 +60,7 @@ func GetDefaultManifest(application string) NaisManifest {
 			},
 		},
 		LeaderElection: false,
+		Redis: false,
 	}
 	defaultManifest.Image = "docker.adeo.no:5000/" + application
 

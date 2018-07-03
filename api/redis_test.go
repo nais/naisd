390f91ce6095bfdef7cecc7ac4ea71e33afc497d
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2018-07-03 10:50:52 +0200

    Test REDIS_HOST is set when redis: true

--- api/redis_test.go
+++ api/redis_test.go
@@ -2,7 +2,9 @@ package api
 
 import (
 	"github.com/nais/naisd/api/app"
+	"github.com/nais/naisd/api/naisrequest"
 	"github.com/stretchr/testify/assert"
+	"k8s.io/api/core/v1"
 	"testing"
 )
 
@@ -11,5 +13,14 @@ func TestRedisResource(t *testing.T) {
 		spec := app.Spec{Application: appName, Environment: environment, Team: "teamBeam"}
 		redisFailoverDef := createRedisFailoverDef(spec)
 		assert.Equal(t, int32(3), redisFailoverDef.Spec.Redis.Replicas, "")
+	})
+
+	t.Run("REDIS_HOST env var should be set when redis: true", func(t *testing.T) {
+		spec := app.Spec{Application: appName, Environment: environment, Team: "teamBeam"}
+		manifest := NaisManifest{Redis: true}
+		env, err := createEnvironmentVariables(spec, naisrequest.Deploy{}, manifest, []NaisResource{})
+
+		assert.NoError(t, err)
+		assert.Contains(t, env, v1.EnvVar{Name: "REDIS_HOST", Value: "rfs-" + spec.Environment})
 	})
 }

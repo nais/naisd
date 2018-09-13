4981be82fa1889b4b455f9522e208c125dddfd33
Kim Tore Jensen <kimtjen@gmail.com>
2018-09-13 12:00:45 +0200

    Change source of proxy settings so that nais can have its own settings

--- api/resourcecreator_test.go
+++ api/resourcecreator_test.go
@@ -1110,7 +1110,7 @@ func TestCheckForDuplicates(t *testing.T) {
 func TestInjectProxySettings(t *testing.T) {
 	spec := app.Spec{Application: appName, Environment: environment, Team: teamName, ApplicationNamespaced: true}
 
-	t.Run("proxy settings not be injected in the pod unless requested through manifest", func(t *testing.T) {
+	t.Run("proxy settings should not be injected in the pod unless requested through manifest", func(t *testing.T) {
 		deploymentRequest := naisrequest.Deploy{
 			Application:           "myapp",
 			Version:               "1",
@@ -1121,16 +1121,16 @@ func TestInjectProxySettings(t *testing.T) {
 			WebProxy: false,
 		}
 
-		os.Setenv("HTTP_PROXY", "foo")
-		os.Setenv("HTTPS_PROXY", "bar")
-		os.Setenv("NO_PROXY", "baz")
+		os.Setenv("NAIS_POD_HTTP_PROXY", "http://foo.bar:1234")
+		os.Setenv("NAIS_POD_NO_PROXY", "baz")
 
 		env, err := createEnvironmentVariables(spec, deploymentRequest, manifest, []NaisResource{})
 
 		assert.Nil(t, err)
-		assert.NotContains(t, env, k8score.EnvVar{Name: "HTTP_PROXY", Value: "foo"})
-		assert.NotContains(t, env, k8score.EnvVar{Name: "HTTPS_PROXY", Value: "bar"})
+		assert.NotContains(t, env, k8score.EnvVar{Name: "HTTP_PROXY", Value: "http://foo.bar:1234"})
+		assert.NotContains(t, env, k8score.EnvVar{Name: "HTTPS_PROXY", Value: "http://foo.bar:1234"})
 		assert.NotContains(t, env, k8score.EnvVar{Name: "NO_PROXY", Value: "baz"})
+		assert.NotContains(t, env, k8score.EnvVar{Name: "JAVA_PROXY_OPTIONS", Value: "-Dhttp.proxyHost=foo.bar -Dhttps.proxyHost=foo.bar -Dhttp.proxyPort=1234 -Dhttps.proxyPort=1234 -Dhttp.nonProxyHosts=baz"})
 	})
 
 	t.Run("proxy settings should be injected in the pod if requested through manifest", func(t *testing.T) {
@@ -1144,19 +1144,19 @@ func TestInjectProxySettings(t *testing.T) {
 			WebProxy: true,
 		}
 
-		os.Setenv("HTTP_PROXY", "foo")
-		os.Setenv("https_proxy", "bar")
-		os.Setenv("NO_PROXY", "baz")
+		os.Setenv("NAIS_POD_HTTP_PROXY", "http://foo.bar:1234")
+		os.Setenv("NAIS_POD_NO_PROXY", "baz")
 
 		env, err := createEnvironmentVariables(spec, deploymentRequest, manifest, []NaisResource{})
 
 		assert.Nil(t, err)
-		assert.Contains(t, env, k8score.EnvVar{Name: "HTTP_PROXY", Value: "foo"})
-		assert.Contains(t, env, k8score.EnvVar{Name: "HTTPS_PROXY", Value: "bar"})
+		assert.Contains(t, env, k8score.EnvVar{Name: "HTTP_PROXY", Value: "http://foo.bar:1234"})
+		assert.Contains(t, env, k8score.EnvVar{Name: "HTTPS_PROXY", Value: "http://foo.bar:1234"})
 		assert.Contains(t, env, k8score.EnvVar{Name: "NO_PROXY", Value: "baz"})
-		assert.Contains(t, env, k8score.EnvVar{Name: "http_proxy", Value: "foo"})
-		assert.Contains(t, env, k8score.EnvVar{Name: "https_proxy", Value: "bar"})
+		assert.Contains(t, env, k8score.EnvVar{Name: "http_proxy", Value: "http://foo.bar:1234"})
+		assert.Contains(t, env, k8score.EnvVar{Name: "https_proxy", Value: "http://foo.bar:1234"})
 		assert.Contains(t, env, k8score.EnvVar{Name: "no_proxy", Value: "baz"})
+		assert.Contains(t, env, k8score.EnvVar{Name: "JAVA_PROXY_OPTIONS", Value: "-Dhttp.proxyHost=foo.bar -Dhttps.proxyHost=foo.bar -Dhttp.proxyPort=1234 -Dhttps.proxyPort=1234 -Dhttp.nonProxyHosts=baz"})
 	})
 }
 

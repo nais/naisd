0ff01effc28465df906e339ff481446f2263fb70
Kim Tore Jensen <kimtjen@gmail.com>
2018-11-22 13:13:14 +0100

    Revert "Add quotes on Java options (#127)"
    This reverts commit 351d134f6da007759d9a34061628a053781cdc28.
    
    See the discussion:
    https://nav-it.slack.com/archives/C71GRG335/p1542888648061200
    
    And this issue:
    https://github.com/navikt/baseimages/pull/32

--- proxyopts/proxyopts_test.go
+++ proxyopts/proxyopts_test.go
@@ -12,27 +12,27 @@ var testCases = []struct {
 }{
 	{
 		proxyUrl: "http://foo.bar:1234",
-		Output:   `-Dhttp.proxyHost='foo.bar' -Dhttps.proxyHost='foo.bar' -Dhttp.proxyPort='1234' -Dhttps.proxyPort='1234'`,
+		Output:   `-Dhttp.proxyHost=foo.bar -Dhttps.proxyHost=foo.bar -Dhttp.proxyPort=1234 -Dhttps.proxyPort=1234`,
 	},
 	{
 		Output: ``,
 	},
 	{
 		proxyUrl: "http://foo.bar:1234",
-		Output:   `-Dhttp.proxyHost='foo.bar' -Dhttps.proxyHost='foo.bar' -Dhttp.proxyPort='1234' -Dhttps.proxyPort='1234'`,
+		Output:   `-Dhttp.proxyHost=foo.bar -Dhttps.proxyHost=foo.bar -Dhttp.proxyPort=1234 -Dhttps.proxyPort=1234`,
 	},
 	{
 		noProxy: "internalhost",
-		Output:  `-Dhttp.nonProxyHosts='internalhost'`,
+		Output:  `-Dhttp.nonProxyHosts=internalhost`,
 	},
 	{
 		noProxy: "host1,host2,.wildcard.local,.local,foo",
-		Output:  `-Dhttp.nonProxyHosts='host1|host2|*.wildcard.local|*.local|foo'`,
+		Output:  `-Dhttp.nonProxyHosts=host1|host2|*.wildcard.local|*.local|foo`,
 	},
 	{
 		proxyUrl: "http://foo.bar:1234",
 		noProxy:  "host1,host2,.wildcard.local,.local,foo",
-		Output:   `-Dhttp.proxyHost='foo.bar' -Dhttps.proxyHost='foo.bar' -Dhttp.proxyPort='1234' -Dhttps.proxyPort='1234' -Dhttp.nonProxyHosts='host1|host2|*.wildcard.local|*.local|foo'`,
+		Output:   `-Dhttp.proxyHost=foo.bar -Dhttps.proxyHost=foo.bar -Dhttp.proxyPort=1234 -Dhttps.proxyPort=1234 -Dhttp.nonProxyHosts=host1|host2|*.wildcard.local|*.local|foo`,
 	},
 	{
 		proxyUrl: "foo.bar:1234",

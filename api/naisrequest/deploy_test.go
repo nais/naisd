1f39f2e3d5a593797042d9644674ad49e0c1d494
Millad Dagdoni <millad.sam.dagdoni@no.ibm.com>
2018-11-22 15:34:38 +0100

    FIX fasit password and username mask on fmt print out

---
+++ api/naisrequest/deploy_test.go
@@ -0,0 +1,23 @@
+package naisrequest
+
+import (
+ 	"encoding/json"
+	"testing"
+     "github.com/nais/naisd/api/naisrequest"
+     "github.com/stretchr/testify/assert"
+ )
+
+ func TestStringMethodInDeployNaisRequestShouldHidePasswordAndUsername(t *testing.T) {
+	  deployRequest := naisrequest.Deploy{
+			FasitUsername: "username" ,
+			FasitPassword: "password",
+			Namespace:     "app",
+		}
+		
+		jsonValue,err := json.Marshal(deployRequest.String())
+		if err != nil{
+			panic(err)
+		}
+		assert.Contains(t, string(jsonValue), "***")
+		assert.Contains(t, string(jsonValue), "fasitPassword")
+ }

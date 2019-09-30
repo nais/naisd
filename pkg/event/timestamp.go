d34c3570ac070e72d07e20c38714571161b68bea
Kim Tore Jensen <kimtjen@gmail.com>
2019-09-30 14:05:42 +0200

    Fix Code Climate warnings

--- pkg/event/timestamp.go
+++ pkg/event/timestamp.go
@@ -4,6 +4,7 @@ import (
 	"time"
 )
 
+// Return the Event's timestamp as standard library format.
 func (m *Event) GetTimestampAsTime() time.Time {
 	return time.Unix(m.GetTimestamp().GetSeconds(), int64(m.GetTimestamp().GetNanos()))
 }
